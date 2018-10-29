b76ff2d627ee052ca5b0f3a698a288b1f04ed1e3
Nicholas Johnson <nbjohnson10@gmail.com>
2018-10-29 16:45:12 -0400

    Cleanup

--- main.py
+++ main.py
@@ -6,13 +6,12 @@ def main():
     motor3 = 111  # need to set
     motor4 = 111  # need to set
 
-    setup(motor1,motor2,motor3,motor4)
+    setup(motor1, motor2, motor3, motor4)
 
 
+def setup(motor1, motor2, motor3, motor4):
 
-def setup(motor1,motor2,motor3,motor4)
-
-    # setup wiringpi and pwm ouputs
+    # setup wiringpi and pwm outputs
     wiringpi.wiringPiSetup()
 
     wiringpi.pinMode(motor1, 2)
@@ -29,15 +28,15 @@ def setup(motor1,motor2,motor3,motor4)
     # wiringpi.softPwmWrite(motor3, 0)
     # wiringpi.softPwmWrite(motor4, 0)
 
-def writeMotor(motor,output):
+def writeMotor(motor, output):
 
     if (output >= 0) and (output <= 100):
-        wiringpi.softPwmWrite(motor,output)
+        wiringpi.softPwmWrite(motor, output)
 
-def writeMotorSoft(motor,output):
+def writeMotorSoft(motor, output):
 
     if (output >= 0) and (output <= 100):
-        wiringpi.softPwmWrite(motor,output)
+        wiringpi.softPwmWrite(motor, output)
 
 
 if __name__ == "__main__":
