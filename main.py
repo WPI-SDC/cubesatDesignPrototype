141c64a5925021a1e191a9eed3472fee86094f61
Nicholas <nbjohnson10@gmail.com>
2018-11-01 23:25:54 +0000

    Troubleshooted pin output, regular pwm working, softpwm work in progress

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import wiringpi
+import time
 
 def main():
-    motor1 = 18
-    motor2 = 13
-    motor3 = 111  # need to set
-    motor4 = 111  # need to set
+    motor1 = 1 # 0-1023
+    motor2 = 24 # 0-1023
+    motor3 = 28 # 0-100
+    motor4 = 29 # 0-100
 
+    wiringpi.wiringPiSetup()
     setup(motor1, motor2, motor3, motor4)
-
+    print("setup done")
+    print("testing motor output")
+    #writeMotor(motor1, 512)
+    writeMotorSoft(motor3, 50)
 
 def setup(motor1, motor2, motor3, motor4):
 
@@ -17,20 +22,19 @@ def setup(motor1, motor2, motor3, motor4):
     wiringpi.pinMode(motor1, 2)
     wiringpi.pinMode(motor2, 2)
 
-    wiringpi.pinMode(motor3, 0)
-    wiringpi.pinMode(motor4, 0)
+    wiringpi.pinMode(motor3, 1)
+    wiringpi.pinMode(motor4, 1)
     wiringpi.softPwmCreate(motor3, 0, 100)
     wiringpi.softPwmCreate(motor4, 0, 100)
 
-    # 0 to 100 range?
-    # wiringpi.pwmWrite(motor1, 0)
-    # wiringpi.pwmWrite(motor2, 0)
-    # wiringpi.softPwmWrite(motor3, 0)
-    # wiringpi.softPwmWrite(motor4, 0)
+    wiringpi.pwmWrite(motor1, 0)
+    wiringpi.pwmWrite(motor2, 0)
+    wiringpi.softPwmWrite(motor3, 0)
+    wiringpi.softPwmWrite(motor4, 0)
 
 def writeMotor(motor, output):
 
-    if (output >= 0) and (output <= 100):
+    if (output >= 0) and (output <= 1023):
         wiringpi.softPwmWrite(motor, output)
 
 def writeMotorSoft(motor, output):
